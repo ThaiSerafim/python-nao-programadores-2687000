5ed431ad70bd8e2dc607d82a2ed1fdceb822172a
ThaiSerafim <166326424+ThaiSerafim@users.noreply.github.com>
2024-10-22 14:35:37 +0000

    variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,8 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+>>> resumo = "Thais é uma mulher de 24 anos que deseja aumentar sua capacidade profissional, por isso está estudando 'Phyton'."
 # Declare variáveis de cada tipo numérico
-
-
+>>> idade = 24
 # Declare uma variável do tipo booleano
+>>> bool(1)
